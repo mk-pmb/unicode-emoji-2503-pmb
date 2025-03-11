d9885705407962f0868c93a8ad76ab8ee9215a22
mk-pmb <mk-pmb@users.noreply.github.com>
2025-03-11 22:12:06 +0100

    v0.1.0: A good start.

---
+++ index.js
@@ -0,0 +1,51 @@
+﻿/* -*- tab-width: 2 -*- */
+'use strict';
+
+const getOwn = require('getown');
+
+const u = {
+  ...require('./uniMisc.js'),
+};
+
+
+function vari(v) { return String.fromCodePoint(0xFE00 + (v - 1)); } /*
+  Unicode variation selectors 1..16 = U+FE00..U+FE0F.
+  The subtraction in parens ensures Number type if v isn't. */
+
+
+
+const emoji = {
+  hammerAndWrench: u.hammerAndWrench + vari(16),
+  pencil: u.memo,
+  scalesOfJustice: u.scales + vari(16),
+  unlock: u.openLock,
+};
+
+
+const customAdditionsAndPlaceholders = {
+  chainsaw: u.carpentrySaw, // actual chainsaw isn't in yet :-(
+  swirl: u.bamumLetterPhaseENgkaami,
+  trash: u.wastebasket,
+  wireframeAngel: u.bamumLetterPhaseDYen,
+};
+
+
+const EX = function loopkup(name) {
+  const c = (getOwn(emoji, name) || getOwn(u, name)
+    || getOwn(customAdditionsAndPlaceholders, name));
+  if (c) { return c; }
+  throw new Error('Attempt to use undefined emoji name: ' + name);
+};
+
+Object.assign(EX, {
+  customAdditionsAndPlaceholders,
+  emoji,
+  unicode: u,
+  vari,
+});
+
+
+
+
+
+module.exports = EX;
